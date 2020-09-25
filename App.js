1e4623dcd2370aac4d93fd0cdc84a012b3ebf485
Felipe <profissionalf.andre@gmail.com>
2020-09-24 22:14:46 -0300

    test: testing git

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import ScreenB from "./src/views/ScreenB";
 import ScreenC from "./src/views/ScreenC";
 
 export default () => {
+  //
   return (
     <SafeAreaView style={styles.viewArea}>
       <ScreenA />
