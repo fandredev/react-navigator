3f3d2dbd002564b57cd855cbbb9b0cb7e2a930b8
Felipe <profissionalf.andre@gmail.com>
2020-09-24 23:58:32 -0300

    feat: add drawer-navigator

--- App.js
+++ App.js
@@ -1,29 +1,22 @@
 import React from "react";
 import { SafeAreaView } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { createDrawerNavigator } from "@react-navigation/drawer";
 import ScreenA from "./src/views/ScreenA";
 import ScreenB from "./src/views/ScreenB";
 import ScreenC from "./src/views/ScreenC";
 
-const Tab = createBottomTabNavigator();
+const Drawer = createDrawerNavigator();
 
 export default () => {
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <NavigationContainer>
-        <Tab.Navigator
-          tabBarOptions={{
-            activeTintColor: "red",
-            inactiveTintColor: "blue",
-            labelStyle: { fontSize: 30 },
-          }}
-          initialRouteName="Tab B"
-        >
-          <Tab.Screen name="Tab A" component={ScreenA} />
-          <Tab.Screen name="Tab B" component={ScreenB} />
-          <Tab.Screen name="Tab C" component={ScreenC} />
-        </Tab.Navigator>
+        <Drawer.Navigator initialRouteName="Tela C">
+          <Drawer.Screen name="Tela A" component={ScreenA} />
+          <Drawer.Screen name="Tela B" component={ScreenB} />
+          <Drawer.Screen name="Tela C" component={ScreenC} />
+        </Drawer.Navigator>
       </NavigationContainer>
     </SafeAreaView>
   );
--- src/views/ScreenC.jsx
+++ src/views/ScreenC.jsx
@@ -1,11 +1,35 @@
 import React from "react";
 import TextView from "../components/TextView";
+import { Button, View, SafeAreaView } from 'react-native'
 
-const ScreenC = ({ route }) => {
+const ScreenC = ({ route, navigation }) => {
   const id = route.params && route.params.number || parseInt(Math.random() * 1000)
   return (
-    <TextView>Screen C - {id}</TextView>
-  )
+    <>
+      <SafeAreaView style={{ flex: 1 }}>
+        <View
+          style={{
+            flexDirection: "row",
+            justifyContent: "flex-end",
+            marginTop: 100,
+          }}
+        >
+          <Button title="Open Drawer" onPress={() => {
+              navigation.openDrawer()
+            setTimeout(() => {
+              navigation.closeDrawer()
+              setInterval(() => {
+                navigation.toggleDrawer()
+              }, 1000)
+            }, 3000)
+          }} />
+        </View>
+      </SafeAreaView>
+      <View style={{ flex: 1 }}>
+        <TextView>Screen C - {id}</TextView>
+      </View>
+    </>
+  );
 };
 
 export default ScreenC;
